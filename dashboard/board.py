ac982dd89cb8b7d28ec9162a8564a7488023303f
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-11-14 16:09:11 -0200

    restructured files

--- dashboard/board.py
+++ dashboard/board.py
@@ -6,19 +6,18 @@ Created on Wed Nov  7 20:35:14 2018
 """
 
 import pandas as pd
+import dbService
 
-from .db import get_engine, get_session
-from .model import Ocorrencia, Recomendacao, Fator, Aeronave
-from datetime import datetime, timedelta
 from flask import Blueprint, render_template
+
 
 bp = Blueprint('board', __name__, url_prefix='/board')
 
 @bp.route('/', methods=('GET',))
 def serve_data():
-    empresa = get_empresa()
-    tipo_operacao = get_tipo_operacao()
-    tipo_aeronave = get_tipo_aeronave()
+    empresa = dbService.get_empresa()
+    tipo_operacao = dbService.get_tipo_operacao()
+    tipo_aeronave = dbService.get_tipo_aeronave()
     
     render_this = {
             'chart1': {'title': 'Ocorrencias por empresa no ultimo ano', 'labels': [str(date.year) + "-" + str(1 + date.month//3) + "T" for date in empresa[-16:].index], 'values': empresa[-16:].to_dict('list')},
@@ -26,30 +25,3 @@ def serve_data():
             'chart3': {'title': '180', 'labels': [str(date.year) + "-" + str(1 + date.month//3) + "T" for date in tipo_aeronave[-16:].index], 'values': tipo_aeronave[-16:].to_dict('list')}
             }
     return render_template('board.html', render_this=render_this)
-
-def get_empresa():
-    df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_fabricante).join(Aeronave).statement, get_engine())
-            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
-            .apply(lambda x: x if x.name != 'aeronave_fabricante' else x.apply(lambda y: 'OUTROS' if y not in ['CESSNA AIRCRAFT', 'EMBRAER', 'PIPER AIRCRAFT', 'AIRBUS INDUSTRIE', 'NEIVA INDUSTRIA AERONAUTICA', 'BEECH AIRCRAFT', 'HELIBRAS', 'AERO BOERO', 'BOEING COMPANY', 'RAYTHEON AIRCRAFT', 'CIRRUS DESIGN', 'AEROSPATIALE AND ALENIA', 'EUROCOPTER FRANCE', 'DIAMOND AIRCRAFT'] else y))
-            .set_index('ocorrencia_dia'))
-    return (pd.get_dummies(df, prefix="", prefix_sep="")
-            .resample('3M')
-            .agg('sum'))
-
-def get_tipo_operacao():
-    df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_tipo_operacao).join(Aeronave).statement, get_engine())
-            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
-            .apply(lambda x: x if x.name != 'aeronave_tipo_operacao' else x.apply(lambda y: 'OUTROS' if y not in ['VOO PRIVADO', 'VOO REGULAR', 'VOO DE INSTRUÇÃO', 'TÁXI AÉREO', 'OPERAÇÃO AGRÍCOLA', 'VOO EXPERIMENTAL'] else y))
-            .set_index('ocorrencia_dia'))
-    return (pd.get_dummies(df, prefix="", prefix_sep="")
-            .resample('3M')
-            .agg('sum'))
-
-def get_tipo_aeronave():
-     df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_nivel_dano).join(Aeronave).statement, get_engine())
-            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
-            .apply(lambda x: x if x.name != 'aeronave_tipo_operacao' else x.apply(lambda y: 'OUTROS' if y not in ['VOO PRIVADO', 'VOO REGULAR', 'VOO DE INSTRUÇÃO', 'TÁXI AÉREO', 'OPERAÇÃO AGRÍCOLA', 'VOO EXPERIMENTAL'] else y))
-            .set_index('ocorrencia_dia'))
-     return (pd.get_dummies(df, prefix="", prefix_sep="")
-             .resample('3M')
-             .agg('sum'))
---
+++ dashboard/dbService.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from db import get_engine, get_session
+from model import Ocorrencia, Recomendacao, Fator, Aeronave
+
+def get_empresa():
+    df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_fabricante).join(Aeronave).statement, get_engine())
+            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
+            .apply(lambda x: x if x.name != 'aeronave_fabricante' else x.apply(lambda y: 'OUTROS' if y not in ['CESSNA AIRCRAFT', 'EMBRAER', 'PIPER AIRCRAFT', 'AIRBUS INDUSTRIE', 'NEIVA INDUSTRIA AERONAUTICA', 'BEECH AIRCRAFT', 'HELIBRAS', 'AERO BOERO', 'BOEING COMPANY', 'RAYTHEON AIRCRAFT', 'CIRRUS DESIGN', 'AEROSPATIALE AND ALENIA', 'EUROCOPTER FRANCE', 'DIAMOND AIRCRAFT'] else y))
+            .set_index('ocorrencia_dia'))
+    return (pd.get_dummies(df, prefix="", prefix_sep="")
+            .resample('3M')
+            .agg('sum'))
+
+def get_tipo_operacao():
+    df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_tipo_operacao).join(Aeronave).statement, get_engine())
+            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
+            .apply(lambda x: x if x.name != 'aeronave_tipo_operacao' else x.apply(lambda y: 'OUTROS' if y not in ['VOO PRIVADO', 'VOO REGULAR', 'VOO DE INSTRUÇÃO', 'TÁXI AÉREO', 'OPERAÇÃO AGRÍCOLA', 'VOO EXPERIMENTAL'] else y))
+            .set_index('ocorrencia_dia'))
+    return (pd.get_dummies(df, prefix="", prefix_sep="")
+            .resample('3M')
+            .agg('sum'))
+
+def get_tipo_aeronave():
+     df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_nivel_dano).join(Aeronave).statement, get_engine())
+            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
+            .apply(lambda x: x if x.name != 'aeronave_tipo_operacao' else x.apply(lambda y: 'OUTROS' if y not in ['VOO PRIVADO', 'VOO REGULAR', 'VOO DE INSTRUÇÃO', 'TÁXI AÉREO', 'OPERAÇÃO AGRÍCOLA', 'VOO EXPERIMENTAL'] else y))
+            .set_index('ocorrencia_dia'))
+     return (pd.get_dummies(df, prefix="", prefix_sep="")
+             .resample('3M')
+             .agg('sum'))
