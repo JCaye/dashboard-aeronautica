a8121a46633beaef157404db2399a9108e373df8
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-11-14 15:51:57 -0200

    simplified code

--- dashboard/board.py
+++ dashboard/board.py
@@ -6,13 +6,11 @@ Created on Wed Nov  7 20:35:14 2018
 """
 
 import pandas as pd
-import functools
 
 from .db import get_engine, get_session
 from .model import Ocorrencia, Recomendacao, Fator, Aeronave
 from datetime import datetime, timedelta
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Blueprint, render_template
 
 bp = Blueprint('board', __name__, url_prefix='/board')
 
@@ -27,36 +25,7 @@ def serve_data():
             'chart2': {'title': 'Total de ocorrencias (eixo esquerdo) e proporcao por tipo de voo (eixo direito)', 'labels': [str(date.year) + "-" + str(1 + date.month//3) + "T" for date in tipo_operacao[-16:].index], 'values': tipo_operacao[-16:].to_dict('list')},
             'chart3': {'title': '180', 'labels': [str(date.year) + "-" + str(1 + date.month//3) + "T" for date in tipo_aeronave[-16:].index], 'values': tipo_aeronave[-16:].to_dict('list')}
             }
-    return render_template('line_chart.html', render_this=render_this)
-
-def get_bar():
-    df =(
-        pd.read_sql(get_session().query(Ocorrencia.codigo_ocorrencia, Ocorrencia.ocorrencia_dia, Aeronave.aeronave_fabricante).join(Aeronave).statement, get_engine())
-            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x))
-            .set_index('ocorrencia_dia'))
-    return (df.loc[df.index > datetime.today() - timedelta(days=180)]
-            .groupby('aeronave_fabricante')
-            .size()
-            .sort_values(ascending=False))
-
-def get_line():
-    df =(
-        pd.read_sql(get_session().query(Ocorrencia.codigo_ocorrencia, Ocorrencia.ocorrencia_dia, Aeronave.aeronave_fabricante).join(Aeronave).statement, get_engine())
-            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x))
-            .set_index('ocorrencia_dia'))
-    bar = get_bar()
-    return (pd.get_dummies(df.drop(columns='codigo_ocorrencia').loc[df.aeronave_fabricante.isin(list(bar[:5].keys())) & (df.index > datetime.today() - timedelta(days=365))],
-                                  prefix="",
-                                  prefix_sep="")
-                .resample('M')
-                .agg('sum')
-                )
-def get_overtime():
-    return (pd.read_sql(get_session().query(Ocorrencia.codigo_ocorrencia, Ocorrencia.ocorrencia_dia).statement, get_engine())
-            .apply(lambda x: x if x.name != 'ocorrencia_dia' else pd.to_datetime(x, yearfirst=True))
-            .set_index('ocorrencia_dia')
-            .resample('3M')
-            .agg('count'))
+    return render_template('board.html', render_this=render_this)
 
 def get_empresa():
     df = (pd.read_sql(get_session().query(Ocorrencia.ocorrencia_dia, Aeronave.aeronave_fabricante).join(Aeronave).statement, get_engine())
