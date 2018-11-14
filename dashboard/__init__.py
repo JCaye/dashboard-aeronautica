11be85dec0699b5c057ba5ba7e768f0741b8dcf6
JULIO CESAR CAYE <juliocaye@gmail.com>
2018-11-14 16:17:40 -0200

    simplified __init__.py

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 
 
@@ -18,12 +16,6 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
     from . import db
     db.init_app(app)
     
